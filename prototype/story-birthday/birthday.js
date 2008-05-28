7a804d793aa6e82535ebba19d08298d6655fb8c4
othree <othree@b19cb30a-9f2e-4084-8847-5e1a13269302>
2008-05-28 17:41:05 +0000

    Update jFormino birthday prototype.
    git-svn-id: http://code.handlino.com/svn/jFormino/branches/story-birthday@437 b19cb30a-9f2e-4084-8847-5e1a13269302

--- prototype/story-birthday/birthday.js
+++ prototype/story-birthday/birthday.js
@@ -3,38 +3,21 @@ function daysInMonth(iMonth, iYear) {
 	return 32 - new Date(iYear, iMonth, 32).getDate();
 }
 
-function getMousePosition (event) {
-    if (document.all) {
-        var x = window.event.clientX + document.documentElement.scrollLeft + $("body")[0].scrollLeft;
-        var y = window.event.clientY + document.documentElement.scrollTop + $("body")[0].scrollTop;
-    } else {
-        var x = event.clientX + window.scrollX;
-        var y = event.clientY + window.scrollY;
-    }
-    return [x,y];
-}
-
-var px;
-var py;
-
 $("#year").focus(function () {
-    $("#birthday-picker").empty().css("top", "").css("left", "");
+    $("#birthday-picker").empty();
     var target = $(this);
-    var hundred = $("<ol></ol>").addClass("year");
-    for (  i = 1910; i <= 2000; i += 10) {
-        var tenCon = $("<li></li>");
-        var ten = $("<ol></ol>");
-        for (  j = 0; j < 10; j += 1) {
+    var hundred = $("<ol>").addClass("year");
+    for (i = 1910; i <= 2000; i += 10) {
+        var tenCon = $("<li>");
+        var ten = $("<ol>");
+        for (j = 0; j < 10; j += 1) {
             var one = $("<li>"+(i+j)+"</li>");
             if (j == 0) one.addClass("first");
             (function () {
                 var val = i+j;
                 one.appendTo(ten);
-                one.click(function (event) {
+                one.click(function () {
                     target.val(val);
-                    var p = getMousePosition(event);
-                    px = p[0];
-                    py = p[1];
                     $("#birthday-picker").hide();
                     $("#month").focus();
                 });
@@ -43,44 +26,38 @@ $("#year").focus(function () {
         ten.appendTo(tenCon);
         tenCon.appendTo(hundred);
     }
-    $("<div class=\"head\">出生年：</div>").appendTo("#birthday-picker");
+    $("<div>出生年：</div>").addClass("head").appendTo("#birthday-picker");
     hundred.appendTo("#birthday-picker");
     $("#birthday-picker").show();
 });
 
-$("#month").focus(function (event) {
+$("#month").focus(function () {
     $("#birthday-picker").empty();
     var target = $(this);
-    var all = $("<ol></ol>").addClass("month");
-    for (  i = 1; i <= 12; i += 1) {
+    var all = $("<ol>").addClass("month");
+    for (i = 1; i <= 12; i += 1) {
         var one = $("<li>"+i+"</li>");
         if (i%4 == 1) one.addClass("one");
         (function () {
             var val = i;
             one.appendTo(all);
-            one.click(function (event) {
+            one.click(function () {
                 target.val(val);
-                var p = getMousePosition(event);
-                px = p[0];
-                py = p[1];
                 $("#birthday-picker").hide();
                 $("#day").focus();
             });
         })();
     }
-    $("<div class=\"head\">出生月：</div>").appendTo("#birthday-picker");
+    $("<div>出生月：</div>").addClass("head").appendTo("#birthday-picker");
     all.appendTo("#birthday-picker");
-
-    var w = $("#birthday-picker").show().width();
-    var h = $("#birthday-picker").show().height();
-    //$("#birthday-picker").css("left", (px-w/2<0)?0:(px-w/2)+"px").css("top", (py-h/2<0)?0:(py-h/2)+"px");
+    $("#birthday-picker").show();
 });
 
 $("#day").focus(function () {
     $("#birthday-picker").empty();
     var target = $(this);
-    var all = $("<ol></ol>").addClass("day");
-    for (  i = 1; i <= daysInMonth($("#month").val()-1, $("#year").val()); i += 1) {
+    var all = $("<ol>").addClass("day");
+    for (i = 1; i <= daysInMonth($("#month").val()-1, $("#year").val()); i += 1) {
         var one = $("<li>"+i+"</li>");
         if (i%7 == 1) one.addClass("one");
         (function () {
@@ -92,9 +69,7 @@ $("#day").focus(function () {
             });
         })();
     }
-    $("<div class=\"head\">出生日：</div>").appendTo("#birthday-picker");
+    $("<div>出生日：</div>").addClass("head").appendTo("#birthday-picker");
     all.appendTo("#birthday-picker");
-    var w = $("#birthday-picker").show().width();
-    var h = $("#birthday-picker").show().height();
-    //$("#birthday-picker").css("left", (px-w/2<0)?0:(px-w/2)+"px").css("top", (py-h/2<0)?0:(py-h/2)+"px");
+    $("#birthday-picker").show();
 });
